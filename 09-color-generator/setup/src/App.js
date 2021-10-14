92f410aab8eec93f239c184b94366759bb1ca2c0
Eduardo S. Libardi <eslibardi@gmail.com>
2021-10-14 14:05:09 -0300

    project 9 - color generator

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,10 +1,79 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
 
-import Values from 'values.js'
+import Values from 'values.js';
 
 function App() {
-  return <h2>color generator project</h2>
+  const [hexColor, setHexColor] = useState('');
+  const [color, setColor] = useState(new Values('#f15025'));
+  const [error, setError] = useState(false);
+
+  const tintsShades = [
+    [100, 0],
+    [90, 0],
+    [80, 0],
+    [70, 0],
+    [60, 0],
+    [50, 0],
+    [40, 0],
+    [30, 0],
+    [20, 0],
+    [10, 0],
+    [0, 0],
+    [0, 10],
+    [0, 20],
+    [0, 30],
+    [0, 40],
+    [0, 50],
+    [0, 60],
+    [0, 70],
+    [0, 80],
+    [0, 90],
+    [0, 100],
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(hexColor);
+      setColor(colors);
+      setError(false);
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  return (
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder='#f15025'
+            value={hexColor}
+            onChange={(e) => setHexColor(e.target.value)}
+            className={`${error ? 'error' : null}`}
+          />
+          <button type='submit' className='btn'>
+            submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        {tintsShades.map((tintShade, index) => {
+          return (
+            <SingleColor
+              key={index}
+              color={color}
+              tint={tintShade[0]}
+              shade={tintShade[1]}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from 'react';
+import rgbToHex from './utils';
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ color, tint, shade }) => {
+  const [alert, setAlert] = useState(false);
 
-export default SingleColor
+  const newColor = color.tint(tint).shade(shade);
+  const hexColor = rgbToHex(...newColor.rgb);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  const copyClipboard = async () => {
+    try {
+      setAlert(true);
+      await navigator.clipboard.writeText(hexColor);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <article
+      onClick={copyClipboard}
+      className={`color ${shade ? 'color-light' : false}  `}
+      style={{ backgroundColor: hexColor }}
+    >
+      <p className='percent-value'>{tint || shade}%</p>
+      <p className='color-value'>{hexColor}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
