8fb9f619565798cf6179e6d71f7f1b911480e2c6
Eduardo S. Libardi <eslibardi@gmail.com>
2021-11-05 13:42:02 -0300

    project 23 - quiz

--- 23-quiz/setup/src/SetupForm.js
+++ 23-quiz/setup/src/SetupForm.js
@@ -1,8 +1,67 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from 'react';
+import { useGlobalContext } from './context';
 
 const SetupForm = () => {
-  return <h2>setup form</h2>
-}
+  const { quiz, handleChange, handleSubmit, error } = useGlobalContext();
 
-export default SetupForm
+  return (
+    <main>
+      <section className='quiz quiz-small'>
+        <form className='setup-form' onSubmit={handleSubmit}>
+          <h2>setup quiz</h2>
+          <div className='form-control'>
+            <label htmlFor='amount'>number of questions</label>
+            <input
+              type='number'
+              name='amount'
+              id='amount'
+              className='form-input'
+              min={1}
+              max={50}
+              value={quiz.amount}
+              onChange={handleChange}
+            ></input>
+          </div>
+          <div className='form-control'>
+            <label htmlFor='category'>category</label>
+            <select
+              name='category'
+              id='category'
+              className='form-input'
+              value={quiz.category}
+              onChange={handleChange}
+            >
+              <option value='sports'>sports</option>
+              <option value='history'>history</option>
+              <option value='politics'>politics</option>
+            </select>
+          </div>
+          <div className='form-control'>
+            <label htmlFor='difficulty'>select difficulty</label>
+            <select
+              name='difficulty'
+              id='difficulty'
+              className='form-input'
+              value={quiz.difficulty}
+              onChange={handleChange}
+            >
+              <option value='easy'>easy</option>
+              <option value='medium'>medium</option>
+              <option value='hard'>hard</option>
+            </select>
+          </div>
+          {error && (
+            <p className='error'>
+              can't generate questions, please try different options
+            </p>
+          )}
+          <button type='submit' className='submit-btn'>
+            start
+          </button>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default SetupForm;
