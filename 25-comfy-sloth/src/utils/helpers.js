0a2ac338a5ecbbfa92a8bfa58590f71b650f7145
Eduardo S. Libardi <eslibardi@gmail.com>
2021-11-11 11:51:21 -0300

    home page - Product component
    format price: Intl number format

--- 25-comfy-sloth/src/utils/helpers.js
+++ 25-comfy-sloth/src/utils/helpers.js
@@ -1,3 +1,8 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(number / 100);
+};
 
-export const getUniqueValues = () => {}
+export const getUniqueValues = () => {};
