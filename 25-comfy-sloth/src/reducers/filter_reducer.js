285ae9f14ca3848b0660a71a536969e8ff44a514
Eduardo S. Libardi <eslibardi@gmail.com>
2021-11-12 21:19:09 -0300

    select GridView or ListView - sort by price and name

--- 25-comfy-sloth/src/reducers/filter_reducer.js
+++ 25-comfy-sloth/src/reducers/filter_reducer.js
@@ -18,6 +18,40 @@ const filter_reducer = (state, action) => {
       filteredProducts: [...action.payload],
     };
   }
+  if (action.type === SET_GRIDVIEW) {
+    return { ...state, gridView: true };
+  }
+
+  if (action.type === SET_LISTVIEW) {
+    return { ...state, gridView: false };
+  }
+
+  if (action.type === UPDATE_SORT) {
+    return { ...state, sort: action.payload };
+  }
+
+  if (action.type === SORT_PRODUCTS) {
+    const { sort, filteredProducts } = state;
+    let tempProducts = [...filteredProducts];
+    if (sort === 'price-lowest') {
+      tempProducts = tempProducts.sort((a, b) => a.price - b.price);
+    }
+    if (sort === 'price-highest') {
+      tempProducts = tempProducts.sort((a, b) => b.price - a.price);
+    }
+    if (sort === 'name-a') {
+      tempProducts = tempProducts.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+    }
+    if (sort === 'name-z') {
+      tempProducts = tempProducts.sort((a, b) => {
+        return b.name.localeCompare(a.name);
+      });
+    }
+    return { ...state, filteredProducts: tempProducts };
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
