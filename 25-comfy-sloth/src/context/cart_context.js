88c8c4169974334c4d3578a3626f9e46a3b3ae2e
Eduardo S. Libardi <eslibardi@gmail.com>
2021-11-15 20:03:58 -0300

    add localStorage to cartItems

--- 25-comfy-sloth/src/context/cart_context.js
+++ 25-comfy-sloth/src/context/cart_context.js
@@ -8,8 +8,17 @@ import {
   COUNT_CART_TOTALS,
 } from '../actions';
 
+const getLocalStorage = () => {
+  let cart = localStorage.getItem('cart');
+  if (cart) {
+    return JSON.parse(localStorage.getItem('cart'));
+  } else {
+    return [];
+  }
+};
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   totalItems: 0,
   totalAmount: 0,
   shippingFee: 534,
@@ -19,6 +28,10 @@ const CartContext = React.createContext();
 
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(state.cart));
+  }, [state.cart]);
 
   // add to cart
   const addToCart = (id, color, amount, product) => {
