7519d5f986cbe58b4e23198596061d9a5c2984a2
Eduardo S. Libardi <eslibardi@gmail.com>
2021-11-18 16:44:44 -0300

    StripeCheckout logic with netlify serverless

---
+++ 25-comfy-sloth/functions/create-payment-intent.js
@@ -0,0 +1,37 @@
+// /domain/.netlify/functions/create-payment-intent
+
+require('dotenv').config();
+const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY);
+
+exports.handler = async function (event, context) {
+  if (event.body) {
+    const { cart, shippingFee, totalAmount } = JSON.parse(event.body);
+
+    const calculateOrderAmount = () => {
+      // Replace this constant with a calculation of the order's amount
+      // Calculate the order total on the server to prevent
+      // people from directly manipulating the amount on the client
+      return shippingFee + totalAmount;
+    };
+
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(),
+        currency: 'brl',
+      });
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ msg: error.message }),
+      };
+    }
+  }
+  return {
+    statusCode: 200,
+    body: 'create payment intent',
+  };
+};
--- 25-comfy-sloth/src/components/StripeCheckout.js
+++ 25-comfy-sloth/src/components/StripeCheckout.js
@@ -18,7 +18,7 @@ const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 const CheckoutForm = () => {
   const { cart, totalAmount, shippingFee, clearCart } = useCartContext();
   const { myUser } = useUserContext();
-  const { history } = useHistory();
+  const history = useHistory();
 
   // Stripe stuff
   const [succeeded, setSucceeded] = useState(false);
@@ -48,7 +48,15 @@ const CheckoutForm = () => {
   };
 
   const createPaymentIntent = async () => {
-    console.log('hello from stripe checkout');
+    try {
+      const { data } = await axios.post(
+        '/.netlify/functions/create-payment-intent',
+        JSON.stringify({ cart, shippingFee, totalAmount })
+      );
+      setClientSecret(data.clientSecret);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   useEffect(() => {
@@ -56,11 +64,45 @@ const CheckoutForm = () => {
     // eslint-disable-next-line
   }, []);
 
-  const handleChange = async (event) => {};
-  const handleSubmit = async (ev) => {};
+  const handleChange = async (event) => {
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : '');
+  };
+  const handleSubmit = async (ev) => {
+    ev.preventDefault();
+    setProcessing(true);
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: { card: elements.getElement(CardElement) },
+    });
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`);
+      setProcessing(false);
+    } else {
+      setError(null);
+      setProcessing(false);
+      setSucceeded(true);
+      setTimeout(() => {
+        clearCart();
+        history.push('/');
+      }, 3000);
+    }
+  };
 
   return (
     <div>
+      {succeeded ? (
+        <article>
+          <h4>Thank you</h4>
+          <h4>Your payment was successful!</h4>
+          <h4>Redirecting to home page shortly</h4>
+        </article>
+      ) : (
+        <article>
+          <h4>Hello, {myUser && myUser.name}</h4>
+          <p>Your total is {formatPrice(shippingFee + totalAmount)}</p>
+          <p>Test Card Number : 4242 4242 4242 4242</p>
+        </article>
+      )}
       <form id='payment-form' onSubmit={handleSubmit}>
         <CardElement
           id='card-element'
