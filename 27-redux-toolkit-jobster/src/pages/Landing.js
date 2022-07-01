f1334f98edce53587851b8d1a68028cb7c9950ad
Eduardo S. Libardi <eslibardi@gmail.com>
2022-07-01 14:31:42 -0300

    Landing page

---
+++ 27-redux-toolkit-jobster/src/pages/Landing.js
@@ -0,0 +1,28 @@
+import logo from '../assets/images/logo.svg';
+import main from '../assets/images/main.svg';
+
+const Landing = () => {
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt='jobster logo' className='logo' />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+            error porro aut aspernatur magnam laboriosam ea quia vel veniam sit?
+          </p>
+          <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </main>
+  );
+};
+
+export default Landing;
