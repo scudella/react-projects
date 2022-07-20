685b32c5a14479061ae3aab4dada98df5935ab61
Eduardo S. Libardi <eslibardi@gmail.com>
2022-07-19 22:23:00 -0300

    BigSidebar

--- 27-redux-toolkit-jobster/src/components/BigSidebar.js
+++ 27-redux-toolkit-jobster/src/components/BigSidebar.js
@@ -1,9 +1,26 @@
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import Logo from './Logo';
+import NavLinks from './NavLinks';
 
 const BigSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
   return (
     <Wrapper>
-      <h2>BigSidebar</h2>;
+      <div
+        className={
+          isSidebarOpen
+            ? 'sidebar-container '
+            : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
