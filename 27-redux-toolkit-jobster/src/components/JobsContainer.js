5cc1178ac1a7f2ddb7f7899e624ffb7af03c30b7
Eduardo S. Libardi <eslibardi@gmail.com>
2022-08-22 08:56:48 -0300

    jobsContainer

---
+++ 27-redux-toolkit-jobster/src/components/JobsContainer.js
@@ -0,0 +1,62 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { getAllJobs } from '../features/allJobs/allJobsSlice';
+import Job from './Job';
+import Loading from './Loading';
+
+const JobsContainer = () => {
+  const { jobs, isLoading } = useSelector((store) => store.allJobs);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllJobs());
+  }, []);
+
+  if (isLoading) {
+    return <Loading center />;
+  }
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <h5>jobs info</h5>
+      <div className='jobs'>
+        {jobs.map((job) => {
+          console.log(job);
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  margin-top: 4rem;
+  h2 {
+    text-transform: none;
+  }
+  & > h5 {
+    font-weight: 700;
+  }
+  .jobs {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 2rem;
+  }
+  @media (min-width: 992px) {
+    .jobs {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      gap: 1rem;
+    }
+  }
+`;
+
+export default JobsContainer;
