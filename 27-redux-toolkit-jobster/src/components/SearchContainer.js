cd7eb7f62ab414287dc0547672d2ca25d30bb626
Eduardo S. Libardi <eslibardi@gmail.com>
2022-08-23 10:18:02 -0300

    initial search setup

--- 27-redux-toolkit-jobster/src/components/SearchContainer.js
+++ 27-redux-toolkit-jobster/src/components/SearchContainer.js
@@ -1,7 +1,109 @@
-import React from 'react';
+import { FormRow, FormRowSelect } from '.';
+import { useSelector, useDispatch } from 'react-redux';
+import styled from 'styled-components';
+import { handleChange, clearFilters } from '../features/allJobs/allJobsSlice';
 
 const SearchContainer = () => {
-  return <h2>SearchContainer</h2>;
+  const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
+    useSelector((store) => store.allJobs);
+  const { jobTypeOptions, statusOptions } = useSelector((store) => store.job);
+  const dispatch = useDispatch();
+
+  const handleSearch = (e) => {
+    dispatch(handleChange({ name: e.target.name, value: e.target.value }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(clearFilters());
+  };
+
+  return (
+    <Wrapper>
+      <form className='form'>
+        <h4>search form</h4>
+        <div className='form-center'>
+          {/* search position */}
+          <FormRow
+            type='text'
+            name='search'
+            value={search}
+            handleChange={handleSearch}
+          />
+          {/* search by status */}
+          <FormRowSelect
+            labelText='status'
+            name='searchStatus'
+            value={searchStatus}
+            handleChange={handleSearch}
+            list={['all', ...statusOptions]}
+          />
+          {/* search by type */}
+          <FormRowSelect
+            labelText='type'
+            name='searchType'
+            value={searchType}
+            handleChange={handleSearch}
+            list={['all', ...jobTypeOptions]}
+          />
+          {/* sort */}
+          <FormRowSelect
+            name='sort'
+            value={sort}
+            handleChange={handleSearch}
+            list={sortOptions}
+          />
+          <button
+            className='btn btn-block btn-danger'
+            disabled={isLoading}
+            onClick={handleSubmit}
+          >
+            clear filters
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  .form {
+    width: 100%;
+    max-width: 100%;
+  }
+  .form-input,
+  .form-select,
+  .btn-block {
+    height: 35px;
+  }
+  .form-row {
+    margin-bottom: 0;
+  }
+  .form-center {
+    display: grid;
+    grid-template-columns: 1fr;
+    column-gap: 2rem;
+    row-gap: 0.5rem;
+  }
+  h5 {
+    font-weight: 700;
+  }
+  .btn-block {
+    align-self: end;
+    margin-top: 1rem;
+  }
+  @media (min-width: 768px) {
+    .form-center {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+  @media (min-width: 992px) {
+    .form-center {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+    .btn-block {
+      margin-top: 0;
+    }
+  }
+`;
 
 export default SearchContainer;
