098b5ef420165008e5d698d2ee2a0ebb61c93638
Eduardo S. Libardi <eslibardi@gmail.com>
2022-08-29 11:24:58 -0300

    fixed unused variables

--- 27-redux-toolkit-jobster/src/pages/dashboard/Stats.js
+++ 27-redux-toolkit-jobster/src/pages/dashboard/Stats.js
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { StatsContainer, Loading, ChartsContainer } from '../../components';
+import { StatsContainer, ChartsContainer } from '../../components';
 import { showStats } from '../../features/allJobs/allJobsSlice';
 
 const Stats = () => {
-  const { isLoading, monthlyApplications } = useSelector(
+  const { monthlyApplications } = useSelector(
     (store) => store.allJobs
   );
   const dispatch = useDispatch();
