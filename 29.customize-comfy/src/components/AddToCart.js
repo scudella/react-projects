6fb2af263c3f1af9a291247d88ab6a5a82f8d7b5
Eduardo S. Libardi <eslibardi@gmail.com>
2023-02-01 16:34:53 -0300

    Replace course rest api for single-product with airtable CMS
    created serverless function to hide the airtable keys from the frontend
    End of project 29

--- 29.customize-comfy/src/components/AddToCart.js
+++ 29.customize-comfy/src/components/AddToCart.js
@@ -63,7 +63,7 @@ const AddToCart = ({ product }) => {
         />
         <Link
           to='/cart'
-          className='btn'
+          className='btn add-to-cart'
           onClick={() => addToCart(id, mainColor, amount, product)}
         >
           add to cart
@@ -116,6 +116,9 @@ const Wrapper = styled.section`
   .btn {
     margin-top: 1rem;
     width: 140px;
+  }
+  .add-to-cart {
+    text-align: center;
   }
 `;
 export default AddToCart;
