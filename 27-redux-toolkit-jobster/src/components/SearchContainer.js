fdc2880e7d41c55b4134191e30688a471b9881b5
Eduardo S. Libardi <eslibardi@gmail.com>
2023-02-02 11:16:04 -0300

    jobster: use debounce to wait for the user stop typing to fetch a new search
    previous mechanism to use isLoading gives a bad user experience

--- 27-redux-toolkit-jobster/src/components/SearchContainer.js
+++ 27-redux-toolkit-jobster/src/components/SearchContainer.js
@@ -2,21 +2,36 @@ import { FormRow, FormRowSelect } from '.';
 import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { handleChange, clearFilters } from '../features/allJobs/allJobsSlice';
+import { useState, useMemo } from 'react';
 
 const SearchContainer = () => {
+  const [localSearch, setLocalSearch] = useState('');
   const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
     useSelector((store) => store.allJobs);
   const { jobTypeOptions, statusOptions } = useSelector((store) => store.job);
   const dispatch = useDispatch();
 
   const handleSearch = (e) => {
-    if (isLoading) return;
     dispatch(handleChange({ name: e.target.name, value: e.target.value }));
   };
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLocalSearch('');
     dispatch(clearFilters());
   };
+
+  const debounce = () => {
+    let timeoutId;
+    return (e) => {
+      setLocalSearch(e.target.value);
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        dispatch(handleChange({ name: e.target.name, value: e.target.value }));
+      }, 1000);
+    };
+  };
+
+  const optimizedDebounce = useMemo(() => debounce(), []);
 
   return (
     <Wrapper>
@@ -27,8 +42,8 @@ const SearchContainer = () => {
           <FormRow
             type='text'
             name='search'
-            value={search}
-            handleChange={handleSearch}
+            value={localSearch}
+            handleChange={optimizedDebounce}
           />
           {/* search by status */}
           <FormRowSelect
